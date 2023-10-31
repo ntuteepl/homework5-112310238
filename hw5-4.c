17ff8f97b908d4f6a8e92cdacdea2c8a93b87903
112310238 <146064544+112310238@users.noreply.github.com>
2023-10-31 14:02:50 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float H,M,a;
+    int hour,min;
+    scanf("%d%d",&hour,&min);
+    H=(hour*30)+min*0.5;
+    M=min*6;
+    if(H>M)
+    {
+        a=H-M;
+    }
+    else a=M-H;
+
+    if(a>180)
+    {
+        a-=180;
+    }
+    printf("%.3f",a);
+
+
+
+}
 
